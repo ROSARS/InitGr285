736141a96e4a77c0bb4447661db06b3b5a201791
Abderrazak Sahraoui <asahraoui@crosemont.qc.ca>
2018-08-28 12:01:58 -0400

    initialisation projet

---
+++ src/b34cr4gitgithub/B34Cr4GitGithub.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package b34cr4gitgithub;
+
+/**
+ *
+ * @author Le Padre
+ */
+public class B34Cr4GitGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
